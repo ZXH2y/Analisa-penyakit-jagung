a5905f6ee480f421e1f77767e05258ed8da9bdba
ZXH2y <152751991+ZXH2y@users.noreply.github.com>
2026-01-03 22:51:49 +0800

    Remove hardcoded user and password in connection.php
    Removed hardcoded database credentials for security.

--- connection.php
+++ connection.php
@@ -1,7 +1,7 @@
 <?php
 $host = 'localhost';
-$user = 'andri';
-$password = '223280019';
+$user = '';
+$password = '';
 $database = 'diagnosa_penyakit_jagung'; 
 
 $connection = mysqli_connect($host, $user, $password, $database);
